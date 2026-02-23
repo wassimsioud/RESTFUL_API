7eb867c39f8163152195cba26b1f4289d7aaed5d
wassimsioud <sioudwassim90@gmail.com>
2026-02-23 13:20:16 +0100

    ajour de la coulonne adresse

--- index.js
+++ index.js
@@ -4,52 +4,41 @@ const app = express();
 app.use(express.json());
 const PORT = 3000;
 app.get('/', (req, res) => {
-res.json("Registre de personnes! Choisissez le bon routage!")
+    res.json("Registre de personnes! Choisissez le bon routage!")
 })
 // Récupérer toutes les personnes
-app.get('/personnes', (req, res) => {
-db.all("SELECT * FROM personnes", [], (err, rows) => {
-if (err) {
-res.status(400).json({
-"error": err.message
-});
-return;
-}
-res.json({
-"message": "success",
-"data": rows
-});
-});
-});
+
 
 
 // Récupérer une personne par ID
 app.get('/personnes/:id', (req, res) => {
-const id = req.params.id;
-db.get("SELECT * FROM personnes WHERE id = ?", [id], (err, row) => {
-if (err) {
-res.status(400).json({
-"error": err.message
-});
-return; }
-res.json({
-"message": "success",
-"data": row });
-});
+    const id = req.params.id;
+    db.get("SELECT * FROM personnes WHERE id = ?", [id], (err, row) => {
+        if (err) {
+            res.status(400).json({
+                "error": err.message
+            });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": row
+        });
+    });
 });
 // Créer une nouvelle personne
 app.post('/personnes', (req, res) => {
-const { nom, adresse } = req.body;
-db.run(`INSERT INTO personnes (nom, adresse) VALUES (?, ?)`, [nom, adresse], function(err) {
-if (err) {
-res.status(400).json({ "error": err.message });
-return;
-}
-res.json({
-"message": "success",
-"data": { id: this.lastID }
-});
-});
+    const { nom, adresse } = req.body;
+    db.run(`INSERT INTO personnes (nom, adresse) VALUES (?, ?)`, [nom, adresse], function (err) {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": { id: this.lastID }
+        });
+    });
 });
 
 
@@ -82,17 +71,18 @@ app.put('/personnes/:id', (req, res) => {
 });
 // Supprimer une personne
 app.delete('/personnes/:id', (req, res) => {
-const id = req.params.id;
-db.run(`DELETE FROM personnes WHERE id = ?`, id, function(err) {
-if (err) {
-res.status(400).json({
-"error": err.message
-});
-return;
-}
-res.json({
-"message": "success" });
-});
+    const id = req.params.id;
+    db.run(`DELETE FROM personnes WHERE id = ?`, id, function (err) {
+        if (err) {
+            res.status(400).json({
+                "error": err.message
+            });
+            return;
+        }
+        res.json({
+            "message": "success"
+        });
+    });
 });
 
-app.listen(PORT, () => {console.log(`Server running on port ${PORT}`); });
+app.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
